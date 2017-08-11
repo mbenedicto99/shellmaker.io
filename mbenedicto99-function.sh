cdef9c87568ba527a0ae0c5214b0a47f589b1fcb
mbenedicto99 <mbenedicto@gmail.com>
2017-08-11 17:09:59 -0300

    commit 20170811-170959

--- mbenedicto99-function.sh
+++ mbenedicto99-function.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-# Functions and parameters
+
+	# Finalidade	: Demonstrar function
+	# Input		: $0
+	# Autor		: Marcos de Benedicto
+	# Data		: 11/08/2017
 
 rc_var () 
 {
 
-   if [ -z "$1" ]                           # Is parameter #1 zero length?
-      then
-          echo "-Parameter #1 is zero length.-"  # Or no parameter passed.
-      else
-          echo "-Parameter #1 is \"$1\".-"
-      fi
+	if [ -z "$1" ]
+	then
+		echo "-Parameter #1 is zero length.-"
+	else
+		echo "-Parameter #1 is \"$1\".-"
+	fi
 
-	variable=${1-$DEFAULT}                   #  What does
-	echo "variable = $variable"              #+ parameter substitution show?
-                                             #  ---------------------------
-                                           #  It distinguishes between
-                                             #+ no param and a null param.
+	variable=${1-$DEFAULT}
+	echo "variable = $variable"
 
-  if [ "$2" ]
-     then
-       echo "-Parameter #2 is \"$2\".-"
-   fi
+  	if [ "$2" ]
+     	then
+       		echo "-Parameter #2 is \"$2\".-"
+   	fi
 
       return 0
  }
-
 
 	rc_var                        # Called with zero-length param
 	rc_var "\$uninitialized_param"   # Called with uninitialized param
